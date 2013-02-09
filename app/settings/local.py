2d32ee1f7162e75b0a1a1a3126af1137a0106ee5
Chris Schomaker <schomaker.c@gmail.com>
2013-02-08 23:34:10 -0500

    Setup

---
+++ app/settings/local.py
@@ -0,0 +1,19 @@
+from .base import *
+
+
+SITE_NAME = 'dev.byebyebirdie:8000'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(REPO_ROOT, 'tmp', 'bikeshop.sqlite'),
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'bikeshop'
+    }
+}
+
